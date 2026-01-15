cf57b2f2b6bb291b0590ef51a54182dc08238f17
atik <thtatikurrahman24@gmail.com>
2026-01-15 16:21:11 +0600

    Pjoject section and funtionality added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Resume Section Buttons Functionality
+// (1) Resume Section Buttons Functionality
 const resumeBtns = document.querySelectorAll('.resume-btn');
 
 resumeBtns.forEach((btn, idx) => {
@@ -18,3 +18,42 @@ resumeBtns.forEach((btn, idx) => {
         resumeDetails[idx].classList.add('active');
     });
 });
+
+
+// (2) project section navigation button functionality
+const arrowRight = document.querySelector('.project-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.project-box .navigation .arrow-left');
+
+let index = 0;
+const activeProject =  () => {
+    const imgSlide = document.querySelector('.project-carousel .img-slide');
+    const projectDetails = document.querySelectorAll('.project-details')
+    projectDetails.forEach(detail => {
+        detail.classList.remove('active')
+    });
+    projectDetails[index].classList.add('active');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+}
+arrowRight.addEventListener('click', () => {
+    if (index < 4) {
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else {
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+    activeProject();
+})
+arrowLeft.addEventListener('click', () => {
+    if (index > 1) {
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else {
+        index = 0;
+        arrowLeft.classList.add('disabled');
+    }
+    activeProject();
+})
