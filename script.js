0b52c738ea1f2c2d867e34b0cd82db1f62d61a5b
atik <thtatikurrahman24@gmail.com>
2026-01-18 16:55:13 +0600

    designed it Resposcive

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 const navLinks = document.querySelectorAll('header nav a');
 const logoLink = document.querySelector('.logo');
 const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+});
 
 
 const activePage = () => {
@@ -26,6 +33,9 @@ const activePage = () => {
         section.classList.remove('active');
     });
 
+
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
 }
 
 navLinks.forEach((link, idx) => {
